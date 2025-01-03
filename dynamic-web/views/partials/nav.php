7862dd67d5a4a2f3e0874d8ad821d446c1166ac4
Nielven Mascariñas <mascarinas022@gmail.com>
2025-01-03 10:00:28 +0800

    Update dynamic-web: making the nav bar dynamic using the global variable to access the route

--- dynamic-web/views/partials/nav.php
+++ dynamic-web/views/partials/nav.php
@@ -8,9 +8,9 @@
                 <div class="hidden md:block">
                     <div class="ml-10 flex items-baseline space-x-4">
                         <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                        <a href="/php/dynamic-web" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Home</a>
-                        <a href="/php/dynamic-web/about.php" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
-                        <a href="/php/dynamic-web/contact.php" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
+                        <a href="/php/dynamic-web" class="<?= urlIs("/php/dynamic-web/") ? 'bg-gray-900 text-white' : 'text-gray-300'?>rounded-md text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
+                        <a href="/php/dynamic-web/about.php" class="<?= urlIs("/php/dynamic-web/about.php") ? 'bg-gray-900 text-white' : 'text-gray-300'?> rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
+                        <a href="/php/dynamic-web/contact.php" class="<?= urlIs("/php/dynamic-web/contact.php") ? 'bg-gray-900 text-white' : 'text-gray-300'?> rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
                     </div>
                 </div>
             </div>
