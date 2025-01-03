6537a6a33b1e730ed527917655674bec394a7b3b
Nielven Mascariñas <mascarinas022@gmail.com>
2025-01-03 13:01:14 +0800

    Updated dynamic-web:Put the controllers on its separate folders

--- dynamic-web/index.php
+++ dynamic-web/index.php
@@ -2,6 +2,13 @@
 
 require "functions.php";
 
-$heading = "Home";
+// Trim `/php/dynamic-web` from the URI
+$uri = str_replace('/php/dynamic-web', '', $_SERVER['REQUEST_URI']);
 
-require "views/index.view.php";
+if ($uri === '/') {
+    require "controllers/index.php";
+} else if ($uri === '/about') {
+    require "controllers/about.php";
+} else if ($uri === '/contact') {
+    require "controllers/contact.php";
+}
