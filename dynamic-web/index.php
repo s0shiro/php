30d02794b47297c8fed238069b0ad275ff72be1a
Nielven Mascariñas <mascarinas022@gmail.com>
2025-01-03 14:08:13 +0800

    dynamic-web: separate the router logic in a separate file and require it in the index and also add a 404 file fallback if the route is not found!.

--- dynamic-web/index.php
+++ dynamic-web/index.php
@@ -1,14 +1,4 @@
 <?php
 
 require "functions.php";
-
-// Trim `/php/dynamic-web` from the URI
-$uri = str_replace('/php/dynamic-web', '', $_SERVER['REQUEST_URI']);
-
-if ($uri === '/') {
-    require "controllers/index.php";
-} else if ($uri === '/about') {
-    require "controllers/about.php";
-} else if ($uri === '/contact') {
-    require "controllers/contact.php";
-}
+require "router.php";
---
+++ dynamic-web/router.php
@@ -0,0 +1,31 @@
+<?php
+
+// Trim `/php/dynamic-web` from the URI
+$uri = parse_url(str_replace('/php/dynamic-web', '', $_SERVER['REQUEST_URI']))['path'];
+
+//routes
+$routes = array("/" => "controllers/index.php",
+    "/about" => "controllers/about.php",
+    "/contact" => "controllers/contact.php"
+);
+
+//handles the routes
+function routeToController($uri, $routes)
+{
+    if (array_key_exists($uri, $routes)) {
+        require $routes[$uri];
+    } else {
+        abort();
+    }
+}
+
+//abort function
+function abort($code = 404)
+{
+    http_response_code($code);
+    require "views/{$code}.view.php";
+    die();
+}
+
+//calling the function to handling routes
+routeToController($uri, $routes);
