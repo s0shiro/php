aaedfd91b0927f340a18231c0b7e5b6b057bf110
Nielven Mascariñas <mascarinas022@gmail.com>
2025-01-05 06:54:58 +0800

    dynamc-web:Refactor the on how the app connect to the database and created a new Class in a separate a file that has a methods on connectiong and querying into the database. Chill guys I know this feels so wrong to query directly into the database, I just go with flow into the lessons.

--- dynamic-web/index.php
+++ dynamic-web/index.php
@@ -1,42 +1,11 @@
 <?php
 
 require "functions.php";
+require "Database.php";
 //require "router.php";
 
-//connect to mysql database
-$dsn = "mysql:host=localhost;port=3306;dbname=myapp;charset=utf8mb4";
+$db = new Database();
+$posts = $db->query("select * from posts where id = 3")->fetch(PDO::FETCH_ASSOC);
 
-$pdo = new PDO($dsn, 'root');
+dd($posts);
 
-$statement = $pdo->prepare("select * from posts");
-$statement-> execute();
-
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-//dd($posts);
-
-foreach ($posts as $post) {
-    echo "<li>" . $post['title'] . "</li>";
-}
-
-
-
-
-
-//sample class
-//class Person {
-//    public $name;
-//    public $age;
-//
-//    public function breath()
-//    {
-//        echo $this -> name . ' is breathing!';
-//    }
-//}
-//
-//$person = new Person();
-//
-//$person -> name = "Niel";
-//$person -> age = 22;
-//
-//$person -> breath();
