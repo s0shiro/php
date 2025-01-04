5257419648ed40d5331d48a9eeba3be395685741
Nielven Mascariñas <mascarinas022@gmail.com>
2025-01-04 14:52:44 +0800

    dynamic-web: Connect the project into a MySQL database and test some queries.

--- dynamic-web/index.php
+++ dynamic-web/index.php
@@ -1,4 +1,42 @@
 <?php
 
 require "functions.php";
-require "router.php";
+//require "router.php";
+
+//connect to mysql database
+$dsn = "mysql:host=localhost;port=3306;dbname=myapp;charset=utf8mb4";
+
+$pdo = new PDO($dsn, 'root');
+
+$statement = $pdo->prepare("select * from posts");
+$statement-> execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+//dd($posts);
+
+foreach ($posts as $post) {
+    echo "<li>" . $post['title'] . "</li>";
+}
+
+
+
+
+
+//sample class
+//class Person {
+//    public $name;
+//    public $age;
+//
+//    public function breath()
+//    {
+//        echo $this -> name . ' is breathing!';
+//    }
+//}
+//
+//$person = new Person();
+//
+//$person -> name = "Niel";
+//$person -> age = 22;
+//
+//$person -> breath();
