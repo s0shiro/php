4efcf4b200ec19e2894c9031625647aa82a62508
Nielven Mascariñas <mascarinas022@gmail.com>
2025-01-12 13:17:08 +0800

    dynamic-web: Create a new route for creating a new note and added a form for adding new note

--- dynamic-web/router.php
+++ dynamic-web/router.php
@@ -1,15 +1,6 @@
 <?php
 
-// Trim `/php/dynamic-web` from the URI
-$uri = parse_url(str_replace('/php/dynamic-web', '', $_SERVER['REQUEST_URI']))['path'];
-
-//routes
-$routes = array("/" => "controllers/index.php",
-    "/about" => "controllers/about.php",
-    "/notes" => "controllers/notes.php",
-    "/note" => "controllers/note.php",
-    "/contact" => "controllers/contact.php"
-);
+$routes = require ("routes.php");
 
 //handles the routes
 function routeToController($uri, $routes): void
@@ -28,6 +19,9 @@ function abort($code = 404)
     require "views/{$code}.view.php";
     die();
 }
+
+// Trim `/php/dynamic-web` from the URI
+$uri = parse_url(str_replace('/php/dynamic-web', '', $_SERVER['REQUEST_URI']))['path'];
 
 //calling the function to handling routes
 routeToController($uri, $routes);
--- dynamic-web/views/notes.view.php
+++ dynamic-web/views/notes.view.php
@@ -1,22 +1,26 @@
-<?php require ("partials/head.php")?>
-<?php require("partials/nav.php")?>
-<?php require ("partials/banner.php")?>
+<?php require("partials/head.php") ?>
+<?php require("partials/nav.php") ?>
+<?php require("partials/banner.php") ?>
 
 
     <main>
         <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <?php foreach ($notes as $note) :?>
-                <ul>
-                  <li>
-                      <a href="/php/dynamic-web/note?id=<?= $note['id'] ?>" class="text-blue-500 hover:underline">
-                        <?= $note['body'] ?>
-                      </a>
-                  </li>
-                </ul>
-            <?php
+            <ul>
+                <?php foreach ($notes as $note) : ?>
+                    <li>
+                        <a href="/php/dynamic-web/note?id=<?= $note['id'] ?>" class="text-blue-500 hover:underline">
+                            <?= $note['body'] ?>
+                        </a>
+                    </li>
+                <?php
                 endforeach;
-            ?>
+                ?>
+            </ul>
+
+            <p class="mt-6 border border-solid border-black w-1/6 p-3 rounded-lg bg-blue-500">
+                <a href="/php/dynamic-web/notes/create" class="text-white hover:text-black">+ Create Note</a>
+            </p>
         </div>
     </main>
 
-<?php require("partials/footer.php")?>
+<?php require("partials/footer.php") ?>
