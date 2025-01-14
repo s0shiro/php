43d14796331bbd360f2b48c285f3a9f73b811584
Nielven Mascariñas <mascarinas022@gmail.com>
2025-01-14 10:25:17 +0800

    dynamic-web: done with creating a new note and also add a client and server side to the input of the user

---
+++ dynamic-web/Validator.php
@@ -0,0 +1,11 @@
+<?php
+
+//pure function
+class Validator
+{
+    public static function string($value, $min = 1, $max = INF): bool {
+        $value = trim($value);
+
+        return strlen($value) >= $min && strlen($value) <= $max;
+    }
+}
--- dynamic-web/controllers/note-create.php
+++ dynamic-web/controllers/note-create.php
@@ -1,6 +1,27 @@
 <?php
 
+require 'Validator.php';
+
+$config = require 'config.php';
+$db = new Database($config['database']);
 
 $heading = "Create Note";
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $errors = [];
+
+    //add a server side validation before submitting into the database
+
+    if(! Validator::string($_POST['body'], 1, 200)) {
+        $errors['body'] = 'A body is required at least 1 character but at maximum of 200 character. Please try again.';
+    }
+
+    if(empty($errors)) {
+        $db->query('INSERT INTO notes(body, user_id) VALUES (:body, :user_id)', [
+            'body' => $_POST['body'],
+            'user_id' => 1
+        ]);
+    }
+}
 
 require ("views/note-create.view.php");
