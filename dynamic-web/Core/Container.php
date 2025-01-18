40c30a19c6bddb8d79874493b3a2e9ccc0841253
Nielven Mascariñas <mascarinas022@gmail.com>
2025-01-18 21:27:12 +0800

    dynamic-web: created a service container for accessing the database

---
+++ dynamic-web/Core/Container.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Core;
+
+class Container
+{
+
+    protected $bindings = [];
+
+    public function bind($key, $func)
+    {
+        $this->bindings[$key] = $func;
+    }
+
+    public function resolve($key)
+    {
+        if (!array_key_exists($key, $this->bindings)) {
+            throw new \Exception("No matching binding found for {$key}");
+        }
+
+        $resolver = $this->bindings[$key];
+
+        return call_user_func($resolver);
+
+    }
+
+}
